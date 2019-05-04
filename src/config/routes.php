c331797a1e3687d4949370138e1d2a3b7084940e
Yusuf Khadijat <yusufkjart@gmail.com>
2019-05-04 09:02:04 +0100

    changes reverted

--- src/config/routes.php
+++ src/config/routes.php
@@ -371,12 +371,14 @@ Router::get('/drafts', function ($request) {
 //videos page
 Router::get('/videos', function ($request) {
     $user = new Ziki\Core\Auth();
-
+    if (!$user->is_logged_in()) {
+        return $user->redirect('/');
+    }
     $directory = "./storage/videos/";
     $ziki = new Ziki\Core\Document($directory);
     $Videos = $ziki->getVideo();
     //print_r($Videos);
-    return $this->template->render('videos.html', ['videos' => $Videos, 'user' => $user]);
+    return $this->template->render('videos.html', ['videos' => $Videos]);
 });
 Router::get('/microblog', function ($request) {
     $user = new Ziki\Core\Auth();
